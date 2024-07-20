6e11cbdb189d0ba35a19d2ceaf2dc7e7c1f9df67
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2024-07-20 16:23:04 +0500

    Time: 54 ms (49.80%), Space: 48.9 MB (51.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -6,32 +6,13 @@
 var strStr = function(haystack, needle) {
     let j=0
     let index = -1;
-    for (let i=0; i<haystack.length; i++){
-        if (haystack[i] === needle[0]){
-            index = i;
-            if (needle.length === 1){
-                return index;
-            }
-            j = i+1;
-            // console.log(index);
-            // console.log("j", j, haystack[j])
-            for (let k=1; k<needle.length; k++){
-                //console.log('h and n', haystack[j], needle[k])
-                if (haystack[j] === needle[k]){
-                    if (k=== needle.length -1) {
-                        return index;
-                        break;
-                    }
-                    else{
-                        j++;
-                        continue;
-                    }
-                }
-                else{
-                    index = -1
-                    break;
-                }
-            }
+    if(haystack === needle){
+        return 0;
+    }
+    
+    for (let i=0; i<=haystack.length - needle.length; i++){
+        if(haystack.substring(i, i+ needle.length) === needle){
+            return i;
         }
     }
     
