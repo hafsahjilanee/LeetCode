a27b731120b2e07734fb66401b5f0ae4fcff7310
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2024-07-20 12:50:19 +0500

    Update 0026-remove-duplicates-from-sorted-array.js

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-let count = 1;
- for (let i=0; i<nums.length-1;){
-   if (nums[i] !== nums[i+1]){
-     count++;
-     i++;
-   }
-   else if (nums[i] === nums[i+1]){
-     nums.splice(i+1,1);
-   }
- }
- return count;
+    for (let i=0; i< nums.length; i++){
+        if (nums[i] === nums[i+1]){
+            nums.splice(i,1);
+            i--;
+        }
+    }
+//console.log(nums);
+return nums.length;
 };
