bfeb57a846ed1841e09473e7212075e0fb0ddc6f
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2024-12-11 16:58:24 +0500

    Time: 0 ms (100.00%), Space: 49.4 MB (41.88%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {   
+    let count = {0:0, 1:0, 2:0};
+    
+    if (nums.length< count.length) {
+        count = new Array(nums.length).fill(0);
+    }
+    
+    for (let i=0; i<nums.length; i++) {
+        count[nums[i]] +=1
+    }
+    
+    // console.log(Object.keys(count).length)
+    
+    let k =0;
+    for (let i=0; i<Object.keys(count).length; i++) {
+        let freq = count[i];
+        for (let j=0; j<freq; j++) {
+            nums[k] = i
+            k++;
+        }
+    }
+    
+};
