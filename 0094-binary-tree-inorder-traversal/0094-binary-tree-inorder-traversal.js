dacdea99be8b7abea5c632abb927cc92b06b9db1
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-01-08 03:48:39 -0500

    Update 0094-binary-tree-inorder-traversal.js

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,22 +11,41 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
+////////////////////////RECURSIVE SOLUTION
+//     let result = [];
+//     if (!root) {
+//         return [];
+//     }
+
+//     return traversal(root, result);
+// };
+
+// const traversal = (root, result) => {
+//     if (!root) {
+//         return;
+//     }
+
+//     traversal(root.left, result);
+//     result.push(root.val);
+//     traversal(root.right, result);
+
+//     return result;
+
+
+//////////////////////////ITERATIVE SOLUTION
+    let current = root;
     let result = [];
-    if (!root) {
-        return [];
+    let stack = [];
+
+    while (current || stack.length>0) {
+        while (current) {
+            stack.push(current);
+            current = current.left;
+        }
+
+        current = stack.pop();
+        result.push(current.val);
+        current = current.right;
     }
-
-    return traversal(root, result);
-};
-
-const traversal = (root, result) => {
-    if (!root) {
-        return;
-    }
-
-    traversal(root.left, result);
-    result.push(root.val);
-    traversal(root.right, result);
-
     return result;
 }
