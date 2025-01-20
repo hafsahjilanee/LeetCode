799bdeb3e890b690b397b666b030f7463e1d0f7c
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-01-19 20:24:03 -0500

    Time: 4 ms (61.18%), Space: 51.9 MB (17.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,18 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
-    let result = [];
     let map = new Map();
-    
-    for (let i= 0; i<nums.length; i++){
-      let number = target - nums[i];
-      //second condition is to make sure both indexes are not same
-      if (map.has(number) && map.get(number) !== i ){
-        result.push(i);
-        result.push(map.get(number))
-        return result
-      }
-      
-      map.set(nums[i], i);
+    let complement =0;
+    let result = [];
+    for (let i=0; i<nums.length; i++) {
+        complement = target - nums[i];
+        if (map.has(complement)) {
+            result.push(i);
+            result.push(map.get(complement));
+            return result;
+        }
+        
+        map.set(nums[i], i);
     }
-    return [];
-    
 };
