fc1423eb2600633462de0f87dbb5b408e6923652
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-02-12 15:36:40 -0500

    Time: 61 ms (74.99%), Space: 53 MB (64.61%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,21 +11,38 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if (!head) {
-        return false;
-    }
-    let visited = new Set();
-    let current = head;
-    while (current) {
-        if (visited.has(current)) {
+    // //initial solution
+    // if (!head) {
+    //     return false;
+    // }
+    // let visited = new Set();
+    // let current = head;
+    // while (current) {
+    //     if (visited.has(current)) {
+    //         return true;
+    //     }
+
+    //     //store the entire current because if you just store value, there may be repetitions of value in ll which may be misleading
+    //     visited.add(current);
+    //     current = current.next;
+
+    // }
+
+    // return false;
+
+    //OPTIMIZED SOLUTION
+    //O(n) time O(1) space
+    let slow= head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow===fast) {
             return true;
         }
-
-        visited.add(current);
-        current = current.next;
-
     }
 
     return false;
-
 };
