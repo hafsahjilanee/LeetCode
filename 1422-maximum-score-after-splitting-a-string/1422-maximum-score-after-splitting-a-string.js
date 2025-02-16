a4aaa994e6dcaffa28e383f8d16f85168afe7e17
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-02-16 18:40:38 -0500

    Time: 71 ms (5.08%), Space: 56.6 MB (5.28%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
@@ -3,24 +3,49 @@
  * @return {number}
  */
 var maxScore = function(s) {
-    if (s.length <=1) {
-        return 1;
-    }
+    //OPTIMIZED TO O(n) time and O(n) space
 
-    let left,right;
-    let zeroCount, oneCount;
+    let oneCount = s.split(1).length - 1;
+    let zeroCount = 0;
     let res =0;
 
-    for (let i=1; i<s.length; i++) {
-        left = s.slice(0,i);
-        right = s.slice(i);
-
-        zeroCount = left.split(0).length-1;
-        oneCount =  right.split(1).length-1;
-        
+    //we do not want to include last element and we want to stop before then hence -1
+    for (let i=0; i<s.length-1; i++) {
+        if (s[i] === '0') {
+            zeroCount +=1;
+        }
+        else {
+            oneCount -= 1;
+        }
+console.log('zerocOUNT AND ONE', zeroCount, oneCount);
         res = Math.max(res, zeroCount+oneCount);
-
     }
 
     return res;
+
+
+
+
+    // if (s.length <=1) {
+    //     return 1;
+    // }
+
+    // let left,right;
+    // let zeroCount, oneCount;
+    // let res =0;
+
+    // for (let i=1; i<s.length; i++) {
+    //     //time complexity O(n^2)
+    //     //space complexity O(n)
+    //     left = s.slice(0,i);
+    //     right = s.slice(i);
+
+    //     zeroCount = left.split(0).length-1;
+    //     oneCount =  right.split(1).length-1;
+        
+    //     res = Math.max(res, zeroCount+oneCount);
+
+    // }
+
+    // return res;
 };
