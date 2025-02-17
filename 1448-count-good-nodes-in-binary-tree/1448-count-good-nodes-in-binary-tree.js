3e60e39cb7e324d75e1dbe6f1322309744e3b598
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-02-16 20:24:08 -0500

    Time: 115 ms (85.96%), Space: 69.3 MB (83.15%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    //we need to maintain max separately for each recursive stack s
+    let x = root.val;
+    let count = 0;
+
+    let dfs = (curr, max) => {
+        if (!curr) {
+            return;
+        }
+        else if (curr.val>=max) {
+            count++;
+            max = curr.val;
+        }
+
+        let left = dfs(curr.left,max);
+        let right = dfs(curr.right,max);
+
+    }
+    //do not call with max 0, initialize max to root.val
+    dfs(root,root.val);
+    return count;
+};
