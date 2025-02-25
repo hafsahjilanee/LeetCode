7fb43629e297d6b522cc6bb7f874dc23ffe8f32e
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-02-25 11:27:19 -0500

    Time: 29 ms (81.7%), Space: 68.4 MB (6.84%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -3,13 +3,12 @@
  * @param {number[]} nums
  */
 var KthLargest = function(k, nums) {
-    //min heap, the root of min heap will always be the k'th largest element
     this.k = k;
-    this.heap =[0];
-    for (const num of nums) {
+    this.q = new MinPriorityQueue();
+
+    for (let num of nums) {
         this.add(num);
     }
-    
 };
 
 /** 
@@ -17,63 +16,13 @@ var KthLargest = function(k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function(val) {
-   if (this.heap.length<this.k+1) {
-    this.heap.push(val);
-    this.perlocateUp()
-   }
-   else if (val > this.heap[1]) {
-    this.heap[1] = val;
-    this.perlocateDown()
-   }
+    this.q.enqueue(val);
+    if (this.q.size()>this.k) {
+        this.q.dequeue();
+    }
 
-   return this.heap[1];
-    
+    return this.q.front();
 };
-
-KthLargest.prototype.perlocateUp = function() {
-    let i = this.heap.length-1;
-    let parentIndex = Math.floor(i/2);
-
-    while (parentIndex>0 && this.heap[parentIndex] > this.heap[i]) {
-        let tmp = this.heap[i];
-        this.heap[i] = this.heap[parentIndex];
-        this.heap[parentIndex] = tmp;
-
-        i= parentIndex;
-        parentIndex= Math.floor(i/2);
-    }
-}
-
-KthLargest.prototype.perlocateDown = function() {
-    if (this.heap.length===1) {
-        return 0;
-    }
-    if (this.heap.length === 2) {
-        return this.heap[1];
-    }
-
-    let i =1;
-
-    while (2*i < this.heap.length) {
-        if (2*i+1 < this.heap.length && this.heap[2*i+1] < this.heap[2*i] && this.heap[i] > this.heap[2*i+1]) {
-            let tmp = this.heap[2*i+1];
-            this.heap[2*i+1] = this.heap[i];
-            this.heap[i] = tmp;
-
-            i=2*i+1
-        }
-        else if (this.heap[i] > this.heap[2*i]) {
-            let tmp = this.heap[2*i];
-            this.heap[2*i] = this.heap[i];
-            this.heap[i] = tmp;
-
-            i=2*i;
-        }
-        else {
-            break;
-        }
-    }
-}
 
 /** 
  * Your KthLargest object will be instantiated and called as such:
