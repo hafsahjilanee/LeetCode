8ccc4f08ddcb8f3b5baf771809ab5d5e0970b743
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-01 02:06:12 -0500

    Time: 7 ms (13.77%), Space: 65.4 MB (8.8%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if (prices.length===0) return 0;
+
+    let profit =0;
+    let buy = prices[0];
+    for (let i=1; i<prices.length; i++) {
+        buy = Math.min(buy, prices[i]); //1
+
+        if (prices[i]>buy) {
+            //also use math.max to keep track of max profit
+            profit = Math.max(profit,prices[i] - buy)
+        }
+    }
+
+    console.log(profit);
+    return profit;
+};
