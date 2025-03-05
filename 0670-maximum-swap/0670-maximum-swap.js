6cf20e4ca4cd6ce4e88cec4b645f28feb3cd38ff
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-05 09:42:43 -0500

    Time: 0 ms (100%), Space: 55 MB (5.51%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function (num) {
+    if (num <= 11) {
+        return num;
+    }
+
+    let numsArr = num.toString().split('');
+
+    let maxRight = numsArr[numsArr.length - 1];
+    let maxRightIndex = numsArr.length - 1;
+
+    let swap_i = -1;
+    let swap_j = -1;
+
+    //find max from right
+    for (let right = numsArr.length - 2; right >= 0; right--) {
+        if (numsArr[right] > maxRight) {
+            maxRight = numsArr[right];
+            maxRightIndex = right;
+        }
+
+        if (numsArr[right] < maxRight) {
+            [swap_i, swap_j] = [right, maxRightIndex];
+        }
+    }
+    [numsArr[swap_i], numsArr[swap_j]] = [numsArr[swap_j], numsArr[swap_i]]
+
+    return Number(numsArr.join(''));
+
+};
