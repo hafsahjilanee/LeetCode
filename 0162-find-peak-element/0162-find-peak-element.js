be93544b84dac5becd15dd129176fe8019ed83d7
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-07 16:09:00 -0500

    Time: 0 ms (100%), Space: 53.8 MB (10.84%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,26 +3,20 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    //time for binary search is O(log n)
-    //space since we're not using any complex data structures O(1)
-    let left = 0;
+    let left =0;
     let right = nums.length-1;
     let middle;
 
-    //are we part of an increasing sequence, move to the right
-    //binary search
-    while (left < right) {  
-        let middle = Math.floor((left + right) / 2);
-
-        //checks whether we are part of an increasing sequence
-        //if so increase the left, bc peak must be on the right
-        if (nums[middle] < nums[middle + 1]) {
-            left = middle + 1;
-        } else {
-        //else if we are part of a decreasing sequence then peak must be on the left
-            right = middle;
+    while (left<right) {
+        middle = Math.floor((left+right)/2);
+        //part of an incr sequence so the peak must be on the right
+        if (nums[middle]<nums[middle+1]) {
+            left = middle+1;
+        }
+        else {
+            right=middle;
         }
     }
 
-    return left; 
+    return left;
 };
