5a65c2ae2de6b63255e97b0a218b2c4d067251cf
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-07 22:51:25 -0500

    Time: 102 ms (72.66%), Space: 112.1 MB (5.05%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,10 +1,26 @@
+class ListNode {
+    constructor(key,value) {
+        this.key = key;
+        this.val = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
 /**
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-    this.length = capacity;
-    this.map = new Map();
-    this.lruArr = [];
+    this.capacity = capacity;
+    this.node_map = new Map();
+
+    //init with some dummy values
+    this.head = new ListNode(-1,-1);
+    this.tail = new ListNode(-1,-1);
+
+    //since it's a doubly linked list we'll need to link them
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
 };
 
 /** 
@@ -12,15 +28,15 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if (this.map.has(key)) {
-        // access the key that is asked and push it to the back of lru array
-        const index = this.lruArr.indexOf(key);
-        this.lruArr.splice(index,1);
-        this.lruArr.push(key);
-        return this.map.get(key);
-    }
-    
-    return -1;
+    if (!this.node_map.has(key)) return -1;
+
+    let node = this.node_map.get(key);
+
+    //since this node has been recently used that means ll needs to be updated and moved to the end of the ll bc it has been recently been used
+    this._remove(node);
+    this._add(node)
+
+    return node.val
 };
 
 /** 
@@ -29,18 +45,38 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    if (this.map.has(key)) {
-        const index = this.lruArr.indexOf(key);
-        this.lruArr.splice(index, 1); 
-    } else if (this.map.size === this.length) {
-        // remove least recently used key
-        const leastUsedKey = this.lruArr.shift(); 
-        this.map.delete(leastUsedKey);          
+    if (this.node_map.has(key)) {
+        let oldNode = this.node_map.get(key);
+        this._remove(oldNode);
     }
-    this.map.set(key, value); 
-    this.lruArr.push(key);    
+
+    let node = new ListNode(key,value);
+    this.node_map.set(key,node);
+    this._add(node);
+
+    if (this.node_map.size > this.capacity) {
+        //remove from head
+        let nodeToDelete = this.head.next;
+        this._remove(nodeToDelete);
+        this.node_map.delete(nodeToDelete.key)
+    }
 };
 
+//add a remove method your self
+LRUCache.prototype._add = function(node) {
+    let prev_end = this.tail.prev;
+    prev_end.next = node;
+    node.prev = prev_end;
+
+    node.next = this.tail;
+    this.tail.prev = node;
+};
+
+//add a remove method your self
+LRUCache.prototype._remove = function(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+};
 
 /** 
  * Your LRUCache object will be instantiated and called as such:
