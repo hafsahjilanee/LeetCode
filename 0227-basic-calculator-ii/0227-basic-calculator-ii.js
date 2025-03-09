d4c07e8628f5c54b0f05ae064d0af6e265f475a8
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-09 17:30:52 -0400

    Time: 26 ms (70.88%), Space: 60.3 MB (47.74%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.js
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -3,57 +3,46 @@
  * @return {number}
  */
 var calculate = function (s) {
-    let prev = 0; // Stores the last processed number (for * and / operations)
-    let curr = 0; // Stores the current number being formed
-    let res = 0;  // Stores the final result
+    let curr_operation = '+';
+    let res = 0;
+    let prev = 0;
+    let sum = 0;
 
-    let curr_operation = '+'; // Tracks the last operation
-    let i = 0;
-
-    while (i < s.length) {
-        let char = s[i];
-
-        // Ignore spaces
-        if (char === ' ') {
-            i++;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
             continue;
         }
-
-        // Extract full number
-        if (!isNaN(char)) {
-            curr = 0;
-            while (i < s.length && !isNaN(s[i]) && s[i] !== ' ') {
-                curr = curr * 10 + parseInt(s[i]);
+        if (!isNaN(s[i])) {
+            let num = 0;
+            while (i < s.length && s[i] !== ' ' && !isNaN(s[i])) {
+                num = num * 10 + parseInt(s[i]);
                 i++;
             }
-            i--; // Step back to correctly process the operator
 
-            // Apply the last stored operation
+            i--;
+
             if (curr_operation === '+') {
-                res += curr;
-                prev = curr;
-            } 
-            else if (curr_operation === '-') {
-                res -= curr;
-                prev = -curr;
-            } 
-            else if (curr_operation === '*') {
-                res -= prev; // Remove prev from result (undo last addition/subtraction)
-                res += prev * curr; // Multiply and add back
-                prev = prev * curr;
-            } 
-            else if (curr_operation === '/') {
-                res -= prev; // Remove prev from result
-                res += Math.trunc(prev / curr); // Integer division
-                prev = Math.trunc(prev / curr);
+                res += num;
+                prev = num;
             }
-        } 
+            else if (curr_operation === '-') {
+                res -= num;
+                prev = -num;
+            }
+            else if (curr_operation === '*') {
+                res -= prev;
+                prev = prev * num;
+                res += prev;
+            } else if (curr_operation === '/') {
+                res -= prev;
+                prev = Math.trunc(prev / num);
+                res += prev;
+            }
+        }
         else {
-            // Update the operation
-            curr_operation = char;
+            curr_operation = s[i];
         }
 
-        i++;
     }
 
     return res;
