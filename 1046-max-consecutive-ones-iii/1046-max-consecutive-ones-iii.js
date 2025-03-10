9ef270dbaa7db235c066dfdd091786e027310886
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-10 14:32:23 -0400

    Time: 4 ms (35.38%), Space: 59.8 MB (28.94%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let left = 0;
+    let max = 0;
+    let zeroCount = 0;
+
+    for (let right=0; right<nums.length; right++) {
+        if (nums[right] === 0) {
+            zeroCount++;
+        }
+        
+        while (zeroCount>k) {
+            if (nums[left] === 0) zeroCount--;
+            left++;
+        }
+        max = Math.max(max, right-left+1);
+    }
+
+    return max;
+};
