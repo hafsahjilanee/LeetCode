201e8ac21c4ce4a0c1abd5b5553201d6b8c96f43
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-09 21:51:44 -0400

    Time: 4 ms (95.27%), Space: 60.6 MB (28.9%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -5,30 +5,27 @@
 var validPalindrome = function(s) {
     let left =0;
     let right = s.length-1;
-
-    //abeeea
-    //abc
-
+//abcdcea
     while (left<right) {
         if (s[left] !== s[right]) {
-            //in slice the last index is not included so we do right+1
-            return isPalindrome(s.slice(left+1,right+1)) || isPalindrome(s.slice(left,right));
+            return isPalindrome(s.slice(left+1,right+1)) || isPalindrome(s.slice(left,right))
         }
+
         left++;
         right--;
     }
-
     return true;
 };
 
-var isPalindrome = function(str) {
-    let left=0;
-    let right=str.length-1;
+function isPalindrome(word) {
+    let left = 0;
+    let right = word.length-1;
 
     while (left<right) {
-        if (str[left] !== str[right]) {
-            return false; 
+        if (word[left] !== word[right]) {
+            return false;
         }
+
         left++;
         right--;
     }
