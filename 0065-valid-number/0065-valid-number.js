efb2d201071aeb19ef82b9e66102733219a4d755
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-10 20:49:18 -0400

    Time: 1 ms (94.36%), Space: 58.1 MB (6.14%) - LeetHub

--- 0065-valid-number/0065-valid-number.js
+++ 0065-valid-number/0065-valid-number.js
@@ -3,5 +3,41 @@
  * @return {boolean}
  */
 var isNumber = function(s) {
-    return /^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/.test(s.trim());
+    s = s.trim();
+    let n = s.length;
+
+    if (n===0) return false;
+
+    let i=0; 
+    let hasNum = false;
+    let hasDot= false;
+    let hasE = false;
+
+    if (s[i]==='-' || s[i]==='+') i++;
+
+    while (i<n) {
+        if (s[i] >= '0' && s[i] <='9') hasNum= true;
+        else if (s[i]==='.') {
+            //if dot already exists return false 
+            //or if you have 6e6.5, can not allow . after e so return false
+            if (hasDot || hasE) return false;
+
+            hasDot = true;
+        }
+        else if (s[i]==='e' || s[i]==='E') {
+            if (!hasNum || hasE) return false;
+
+            hasE = true;
+            hasNum = false //reset to check for number after
+
+            //to handle cases where e followed by +- e.g 3e+7 or 3e-6
+            if (i + 1 < n && (s[i + 1] === '+' || s[i + 1] === '-')) i++;
+        }
+        else {
+            return false;
+        }
+        i++
+    }
+
+    return hasNum;
 };
