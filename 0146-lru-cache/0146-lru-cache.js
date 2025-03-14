65e344ce749a93006c019544e232e771d55e95f2
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-13 22:57:10 -0400

    Time: 122 ms (17.14%), Space: 110.6 MB (11.06%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,40 +1,43 @@
 class ListNode {
-    constructor(key, value) {
-        this.val = value;
+    constructor(key,val) {
         this.key = key;
+        this.val = val;
         this.prev = null;
         this.next = null;
     }
 }
-//push towards the end
-//remove from the head
+
 /**
  * @param {number} capacity
  */
-var LRUCache = function (capacity) {
-    this.map = new Map();
-    //doubly linked list
-    this.head = new ListNode(-1, -1);
-    this.tail = new ListNode(-1, -1);
+var LRUCache = function(capacity) {
     this.capacity = capacity;
+    this.map = new Map();
+
+    this.head = new ListNode(-1,-1);
+    this.tail = new ListNode(-1,-1);
 
     this.head.next = this.tail;
-    this.tail.prev = this.head
+    this.tail.prev = this.head;
 };
 
 /** 
  * @param {number} key
  * @return {number}
  */
-LRUCache.prototype.get = function (key) {
-    //fetch from the map
-    //move it to the back
+LRUCache.prototype.get = function(key) {
+    //check if map has key
+    //add key to back of ll since it was recently used
     if (!this.map.has(key)) return -1;
 
-    let node = this.map.get(key);
-    this._remove(node);
-    this._add(node);
-    return node.val;
+    if (this.map.has(key)) {
+        let oldNode = this.map.get(key);
+        this._remove(oldNode);
+        this._add(oldNode);
+    }
+
+    return this.map.get(key).val;
+    
 };
 
 /** 
@@ -42,26 +45,41 @@ LRUCache.prototype.get = function (key) {
  * @param {number} value
  * @return {void}
  */
-LRUCache.prototype.put = function (key, value) {
-    //check capacity
-    //and then remove and add
+LRUCache.prototype.put = function(key, value) {
+    // If the key already exists, remove it to update its value and move it to the back
     if (this.map.has(key)) {
         let oldNode = this.map.get(key);
         this._remove(oldNode);
-
     }
 
+    //add node to back of ll
+    //check if capacity is exceeding then remove
     let node = new ListNode(key,value);
-    this.map.set(key, node);
     this._add(node);
+    this.map.set(key, node);
 
     if (this.map.size > this.capacity) {
-        let lruNode = this.head.next;
+        //remove from head
+        let lruNode = this.head.next
+        this.map.delete(lruNode.key);
         this._remove(lruNode);
-        this.map.delete(lruNode.key)
     }
-
+    
 };
+
+LRUCache.prototype._add = function(node) {
+    let prevNode = this.tail.prev;
+    prevNode.next = node;
+    node.prev = prevNode;
+
+    node.next = this.tail;
+    this.tail.prev = node;
+}
+
+LRUCache.prototype._remove = function(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+}
 
 /** 
  * Your LRUCache object will be instantiated and called as such:
@@ -69,22 +87,3 @@ LRUCache.prototype.put = function (key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
-
-//_remove
-LRUCache.prototype._remove = function (node) {
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
-};
-
-//_add
-LRUCache.prototype._add = function (node) {
-    //add to the end of the ll
-    let prevNode = this.tail.prev;
-    prevNode.next = node;
-    node.prev = prevNode;
-    node.next = this.tail;
-    this.tail.prev = node;
-
-
-
-};
