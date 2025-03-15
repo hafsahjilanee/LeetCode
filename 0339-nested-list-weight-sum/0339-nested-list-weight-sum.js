8fa1b7cad1160978c6c88bbb62c4f0eca306d1b8
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-15 03:37:20 -0400

    Time: 0 ms (100%), Space: 54.4 MB (13.92%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.js
@@ -0,0 +1,65 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     The result is undefined if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     @return {void}
+ *     this.setInteger = function(value) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     @return {void}
+ *     this.add = function(elem) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     The result is undefined if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @param {NestedInteger[]} nestedList
+ * @return {number}
+ */
+var depthSum = function(nestedList) {
+    let res = 0;
+    let depth = 1;
+    let q = [];
+    q.push(...nestedList);
+
+    while (q.length) {
+        let size = q.length;
+        for (let i=0; i<size; i++) {
+            let curr = q.shift();
+
+            if (curr.isInteger()) {
+                res+= curr.getInteger()*depth;
+            }
+            else {
+                q.push(...curr.getList());
+            }
+        }
+        depth++;
+    }
+
+    return res;
+};
