f2c70e02e4de6d5cd3770177a1eb543d619a6157
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-17 14:04:05 -0400

    Time: 0 ms (100%), Space: 53.8 MB (19.92%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.js
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.js
@@ -4,30 +4,36 @@
  * @return {boolean}
  */
 var validWordAbbreviation = function(word, abbr) {
+    //space O(N) space
+    //O(1) time
+
     let wordPtr = 0;
     let abbrPtr = 0;
     let steps = 0;
+    if (word.length < abbr.length) {
+        return false;
+    }
 
-    while (wordPtr<word.length && abbrPtr<abbr.length) {
+    while (wordPtr < word.length && abbrPtr < abbr.length) {
         if (!isNaN(abbr[abbrPtr])) {
             steps = 0;
-
-            if (abbr[abbrPtr] === '0') return false;
-            
-            while (abbrPtr<abbr.length && !isNaN(abbr[abbrPtr])) {
+            if (abbr[abbrPtr] === '0') {
+                return false;
+            }
+            while (abbrPtr < abbr.length && !isNaN(abbr[abbrPtr])) {
                 steps = steps*10 + Number(abbr[abbrPtr]);
                 abbrPtr++;
             }
-            
-            wordPtr+= steps;
+
+            wordPtr += steps;
         }
         else {
-            if (abbr[abbrPtr] !== word[wordPtr]) return false
-
-            abbrPtr++;
+            if (word[wordPtr] !== abbr[abbrPtr]) {
+                return false;
+            }
             wordPtr++;
+            abbrPtr++;
         }
     }
-
     return wordPtr === word.length && abbrPtr === abbr.length;
 };
