065e6d9a6fd2ab040695d43f5b833b256d617c07
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-17 13:26:16 -0400

    Time: 0 ms (100%), Space: 54.7 MB (18.3%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.js
+++ 0031-next-permutation/0031-next-permutation.js
@@ -3,37 +3,45 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    if (!nums.length) return [];
+    //time O(n)
+    //space O(1) bc we are swapping in place
     let pivot = -1;
+
     //find pivot
-    for (let right=nums.length; right>=0; right--) {
+    //321
+    for (let right = nums.length-1; right>0; right--) {
         //14587
-        if (nums[right]>nums[right-1]) {
+        if (nums[right-1] < nums[right]) {
             pivot = right-1;
-            break;
+            break
         }
     }
 
-    //if no pivot found 321 => 123
-    if (pivot===-1) {
+    if (pivot === -1) {
         nums.reverse();
         return;
     }
 
-    //swap with first number greater than pivot
-    for (let right=nums.length; right>pivot; right--) {
+    //14587
+    for (let right = nums.length-1; right>pivot; right--) {
         //14587 => 14785
         if (nums[right]> nums[pivot]) {
-            [nums[right], nums[pivot]] = [nums[pivot],nums[right]];
+            [nums[right], nums[pivot]] = [nums[pivot], nums[right]];
             break;
         }
     }
 
-    //swap all numbers after the pivot index
-    let left = pivot+1, right=nums.length-1;
-    while (left<right) {
-        [nums[left],nums[right]] = [nums[right], nums[left]];
+    //swap everything after the pivot
+    //14785 => 14758
+    let left = pivot +1;
+    let right = nums.length-1;
+
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
         left++;
         right--;
     }
+     
+
+
 };
