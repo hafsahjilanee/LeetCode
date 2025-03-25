148f6907d26ee1306d527493a103d7a13126bf85
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-25 15:36:10 -0400

    Time: 2 ms (67.94%), Space: 63.5 MB (43.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let left = 0;
+    let max = nums[0];
     let sum = 0;
-    let max = -Infinity;
 
-    for (let right=0; right<nums.length; right++) {
-        sum+=nums[right];
+    for (let i=0; i<nums.length; i++) {
+        sum += nums[i];
 
         if (sum>max) {
             max = sum;
         }
 
         if (sum<0) {
-            sum = 0
+            sum=0;
         }
-
     }
 
     return max;
