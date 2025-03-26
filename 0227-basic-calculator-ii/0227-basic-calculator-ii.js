2cc57794ea86540ea807a05fa02f88f6cd7c1d74
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-26 17:22:11 -0400

    Time: 30 ms (53.57%), Space: 60.8 MB (56.73%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.js
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -2,48 +2,45 @@
  * @param {string} s
  * @return {number}
  */
-var calculate = function (s) {
-    s = s.trim();
+var calculate = function(s) {
+    let str = s.trim();
     let curr_operation = '+';
-    let res =0;
+    let res = 0;
     let prev = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === ' ') continue;
-        if (!isNaN(s[i]) && Number(s[i]!==0)) {
-            let number = 0;
-
-            while (i < s.length && s[i] !==' ' && !isNaN(s[i])) {
-                number = number*10 + Number(s[i]);
+    for (let i=0; i<str.length; i++) {
+        if (str[i] === ' ') continue;
+        if (!isNaN(str[i])) {
+            let number =0;
+            //extract the entire number
+            while (i < str.length && str[i] !== ' ' && !isNaN(str[i])) {
+                number = number*10 + parseInt(str[i]);
                 i++;
             }
             i--;
 
             if (curr_operation === '+') {
-                res+=number;
+                res += number;
                 prev = number;
             }
             else if (curr_operation === '-') {
-                res-=number;
-                prev=-number
+                res -= number;
+                prev = -number;
 
             }
             else if (curr_operation === '*') {
-                //3+2*2
-                res -=prev //5-2 = 3
+                res -= prev;
                 prev = prev*number;
                 res += prev;
             }
             else if (curr_operation === '/') {
-                //3+5/2
-                res-=prev; //8-5 = 3
+                res -=prev;
                 prev = Math.trunc(prev/number);
-                res+= prev;
+                res += prev;
             }
-
         }
         else {
-            curr_operation = s[i];
+            curr_operation = str[i];
         }
     }
 
