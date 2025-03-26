4f5b3a3874af94f73a68968064c0cca73f3b46c7
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-26 19:58:37 -0400

    Time: 23 ms (11.5%), Space: 67.3 MB (5.26%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,21 +3,22 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (intervals.length === 0) return [];
+    if (!intervals.length) return [];
 
-    intervals.sort((a,b)=> a[0]-b[0]);
+    let sortedIntervals = intervals.sort((a,b)=> a[0] - b[0]);
+    let res = [sortedIntervals[0]];
 
-    let res = [intervals[0]];
-
-    for (let i=1; i<intervals.length; i++) {
-        let [start, end] = intervals[i];
+    for (let i =1; i<sortedIntervals.length; i++) {
+        //[2,6], [5,8]
+        let [start, end] = sortedIntervals[i];
+        //[1,3]
         let [prevStart, prevEnd] = res[res.length-1];
 
         if (start <= prevEnd) {
             res[res.length-1] = [prevStart, Math.max(end, prevEnd)];
         }
         else {
-            res.push([start, end]);
+            res.push([start,end]);
         }
     }
 
