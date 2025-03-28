157afade0cb7e3cc3cd8dde3ef6b689e4159c787
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-27 22:39:41 -0400

    Time: 79 ms (47.26%), Space: 70.3 MB (32.63%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -9,58 +9,84 @@ var findKthLargest = function (nums, k) {
     //total time O(n log k) time
     //space O(k) bc the heap only stores k elements at a time
 
-    // let q = new MinPriorityQueue();
+    let q = new MinPriorityQueue();
 
-    // for (let num of nums) {
-    //     if (q.size() >= k) {
-    //         if (num < q.front()) {
-    //             continue;
+    for (let num of nums) {
+        q.enqueue(num);
+
+        if (q.size() > k) {
+            q.dequeue();
+        }
+    }
+
+    return q.front();
+
+    //TRYING WITH QUICK SELECTTT
+    // let kIndex = nums.length - k //position at which our largest element is located
+
+    // //if kIndex === pivotIndex return nums[pivotIndex]
+
+    // const quickSelect = (l, r) => {
+    //     let pivot = nums[r];
+    //     let p = l;
+
+    //     //we do nums.length-1 bc we have to skip the last index
+    //     for (let i=l; i<r; i++) {
+    //         if(nums[i]<=pivot) {
+    //             //swap
+    //             [nums[i],nums[p]] = [nums[p], nums[i]];
+    //             p++;
     //         }
-    //         else {
-    //             q.dequeue();
-    //             q.enqueue(num);
-    //         }
+    //     }
+    //     //now swap p with pivot
+    //     [nums[p], nums[r]] = [nums[r], nums[p]];
+
+    //     if (p===kIndex) {
+    //         return nums[p];
+    //     }
+    //     else if (p<kIndex) { //search right partition
+    //         return quickSelect(p+1,r);
     //     }
     //     else {
-    //         q.enqueue(num)
+    //         return quickSelect(l,p-1);
     //     }
+
     // }
 
-    // return q.front();
+    // return quickSelect(0, nums.length-1)
 
-    
-    let kIndex = nums.length - k; // Convert k-th largest to index
+    // let kIndex = nums.length - k; // Convert k-th largest to index
 
-    const quickSelect = (l, r) => {
-        if (l >= r) return nums[l]; // Base case
+    // const quickSelect = (l, r) => {
+    //     if (l >= r) return nums[l]; // Base case
         
-        let pivot = nums[r]; // Choose last element as pivot
-        let lt = l;  // Pointer for less than pivot
-        let gt = r;  // Pointer for greater than pivot
-        let i = l;
+    //     let pivot = nums[r]; // Choose last element as pivot
+    //     let lt = l;  // Pointer for less than pivot
+    //     let gt = r;  // Pointer for greater than pivot
+    //     let i = l;
 
-        while (i <= gt) {
-            if (nums[i] < pivot) {
-                [nums[i], nums[lt]] = [nums[lt], nums[i]];
-                lt++;
-                i++;
-            } else if (nums[i] > pivot) {
-                [nums[i], nums[gt]] = [nums[gt], nums[i]];
-                gt--;
-            } else {
-                i++; // Skip when nums[i] == pivot
-            }
-        }
+    //     while (i <= gt) {
+    //         if (nums[i] < pivot) {
+    //             [nums[i], nums[lt]] = [nums[lt], nums[i]];
+    //             lt++;
+    //             i++;
+    //         } else if (nums[i] > pivot) {
+    //             [nums[i], nums[gt]] = [nums[gt], nums[i]];
+    //             gt--;
+    //         } else {
+    //             i++; // Skip when nums[i] == pivot
+    //         }
+    //     }
 
-        // Now, check where kIndex lies
-        if (kIndex >= lt && kIndex <= gt) {
-            return nums[kIndex];
-        } else if (kIndex < lt) {
-            return quickSelect(l, lt - 1); // Search left
-        } else {
-            return quickSelect(gt + 1, r); // Search right
-        }
-    };
+    //     // Now, check where kIndex lies
+    //     if (kIndex >= lt && kIndex <= gt) {
+    //         return nums[kIndex];
+    //     } else if (kIndex < lt) {
+    //         return quickSelect(l, lt - 1); // Search left
+    //     } else {
+    //         return quickSelect(gt + 1, r); // Search right
+    //     }
+    // };
 
-    return quickSelect(0, nums.length - 1);
+    // return quickSelect(0, nums.length - 1);
 };
