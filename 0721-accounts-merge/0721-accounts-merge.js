3899edb68a4df5958edb1d37f21854951e21afa4
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-30 22:31:58 -0400

    Time: 55 ms (45.56%), Space: 73.5 MB (15.79%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -0,0 +1,55 @@
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function (accounts) {
+    //TC
+    //SC
+    //key email, value: all emails it's connected to
+    let graph = new Map();
+    let emailToName = {};
+
+    for (const account of accounts) {
+        const name = account[0];
+
+        for (let i = 1; i < account.length; i++) {
+            const email = account[i];
+            emailToName[email] = name;
+
+            if (!graph.has(email)) {
+                graph.set(email, new Set());
+            };
+
+            graph.get(email).add(account[1]);
+            graph.get(account[1]).add(email);
+        }
+    }
+
+    let res = [];
+    let visited = new Set();
+
+    for (const email of graph.keys()) {
+        if (!visited.has(email)) {
+            let stack = [email];
+            visited.add(email);
+
+            let localRes = [];
+
+            while (stack.length) {
+                let node = stack.pop();
+
+                localRes.push(node);
+
+                for (const edge of graph.get(node)) {
+                    if (!visited.has(edge)) {
+                        stack.push(edge);
+                        visited.add(edge);
+                    }
+                }
+            }
+
+            res.push([emailToName[email], ...localRes.sort()])
+        }
+    }
+    return res;
+};
