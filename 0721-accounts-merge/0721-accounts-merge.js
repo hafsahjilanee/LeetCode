a31b4ff74c87a7b86639fb5d64ca3109c0d8eb54
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-30 23:07:30 -0400

    Time: 74 ms (23.85%), Space: 73 MB (16.94%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.js
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -3,41 +3,38 @@
  * @return {string[][]}
  */
 var accountsMerge = function (accounts) {
-    //TC O(N × K log K)
-    //SC O(N × K)
-    //key email, value: all emails it's connected to
     let graph = new Map();
     let emailToName = {};
+
+    //treat each email as a node and create a map of the other nodes(emails) it's connected to
 
     for (const account of accounts) {
         const name = account[0];
 
         for (let i = 1; i < account.length; i++) {
-            const email = account[i];
-            emailToName[email] = name;
-
+            let email = account[i]
             if (!graph.has(email)) {
                 graph.set(email, new Set());
-            };
+            }
 
-            graph.get(email).add(account[1]);
             graph.get(account[1]).add(email);
+            graph.get(email).add(account[1]);
+            emailToName[email] = name;
         }
     }
 
-    let res = [];
+    //run dfs
     let visited = new Set();
+    let res = [];
 
     for (const email of graph.keys()) {
         if (!visited.has(email)) {
             let stack = [email];
             visited.add(email);
-
             let localRes = [];
 
             while (stack.length) {
                 let node = stack.pop();
-
                 localRes.push(node);
 
                 for (const edge of graph.get(node)) {
@@ -47,9 +44,9 @@ var accountsMerge = function (accounts) {
                     }
                 }
             }
-
-            res.push([emailToName[email], ...localRes.sort()])
+            res.push([emailToName[email], ...localRes.sort()]);
         }
     }
+
     return res;
 };
