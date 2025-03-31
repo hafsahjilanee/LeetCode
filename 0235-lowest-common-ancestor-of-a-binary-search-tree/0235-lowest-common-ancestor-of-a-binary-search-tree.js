b46c62cbfcf3c888fd3f2a817c961714da7d757a
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-03-31 02:33:51 -0400

    Time: 66 ms (47.49%), Space: 65.9 MB (10.69%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,20 +14,20 @@
  */
 var lowestCommonAncestor = function(root, p, q) {
     let curr = root;
+
     while (true) {
-        if ((curr.val === p.val)|| (curr.val === q.val)) {
+        if (p===curr || q===curr) {
             return curr;
         }
 
-        if ((p.val<curr.val && q.val>curr.val) || (p.val>curr.val && q.val<curr.val)) {
+        if ((p.val < curr.val && q.val>curr.val) || (q.val<curr.val && p.val>curr.val)) {
             return curr;
         }
-        else if (p.val< curr.val && q.val<curr.val) {
+        else if (p.val<curr.val) {
             curr = curr.left;
         }
         else {
             curr = curr.right;
         }
-
     }
 };
