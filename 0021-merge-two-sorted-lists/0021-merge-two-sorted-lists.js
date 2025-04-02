16ded2d97d833af4ab1952b9a1a3c68d3569d275
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-02 12:16:33 -0400

    Time: 0 ms (100%), Space: 57.8 MB (35.5%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,6 +11,8 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
+    //TC O(n + m) Iterates through both lists once.
+    //SC O(1) Only a few pointers are used (dummy, curr).
     if (!list1) return list2;
     if (!list2) return list1;
 
@@ -20,23 +22,17 @@ var mergeTwoLists = function(list1, list2) {
     while (list1 && list2) {
         if (list1.val<list2.val) {
             curr.next = list1;
-            curr = list1;
             list1 = list1.next;
         }
         else {
             curr.next = list2;
-            curr = list2;
             list2 = list2.next;
         }
+        curr = curr.next;
     }
 
-    if (list1) {
-        curr.next = list1
-    }
-
-    if (list2) {
-        curr.next = list2;
-    }
+    // Attach the remaining nodes
+    curr.next = list1 || list2;
 
     return dummy.next;
 };
