58dfb2005c1f5a1c2ae94e9baca6ea3c774428a0
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-02 12:27:05 -0400

    Time: 17 ms (48.82%), Space: 63.8 MB (20.64%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -10,23 +10,28 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-    if (!lists || lists.length===0) return null;
+    //TC O(Nlogk)
+    //SC In the worst case, the heap holds all k elements at once => O(k)
+    if (!lists.length) return null;
 
     let minHeap = new PriorityQueue((a,b) => a.val-b.val);
 
     for (const list of lists) {
-        if (list) minHeap.enqueue(list);
-    }
+        //if condition to check if we ever get an empty list [[]]
+       if (list) minHeap.enqueue(list);
+    };
 
-    let dummy = new ListNode(0);
-    let curr = dummy;
-
+    let dummy = new ListNode(-1);
+    let curr = dummy; 
+    
     while (minHeap.size()>0) {
         let node = minHeap.dequeue();
         curr.next = node;
-        curr = curr.next;
+        curr = node;
 
-        if (node.next) minHeap.enqueue(node.next);
+        if (node.next) {
+            minHeap.enqueue(node.next);
+        }
     }
 
     return dummy.next;
