49f97291a783817260c2dd85b899e18841387b82
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-03 19:38:10 -0400

    Time: 0 ms (100%), Space: 53.4 MB (44.03%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number[][]}
+ */
+var findMissingRanges = function(nums, lower, upper) {
+    if (!nums.length) return [[lower,upper]];
+
+    let missingRanges = [];
+
+    if (lower < nums[0]) {
+        missingRanges.push([lower, nums[0]-1]);
+    }
+    for (let i=1; i<nums.length; i++) {
+        let low = Infinity;
+        let max = Infinity;
+        if (Math.abs(nums[i]-nums[i-1]) !== 1) {
+            low = nums[i-1] +1;
+            high = nums[i] - 1;
+            missingRanges.push([low,high]);
+        }
+    }
+    if (upper>nums[nums.length-1]) {
+        missingRanges.push([nums[nums.length-1]+1, upper]);
+    }
+ return missingRanges;
+};
