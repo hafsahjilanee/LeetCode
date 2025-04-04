c589ed27c08808296c6a84c4f6e1ae8382e639dd
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-04 11:47:56 -0400

    Time: 10 ms (54.54%), Space: 58.8 MB (24.27%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -4,22 +4,27 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    let left=1;
+    let left = 1;
     let right = Math.max(...piles);
-    let middle;
-    let hoursTaken;
-    
-    while (left <= right) {
-        middle = Math.floor((left+right)/2);
-        hoursTaken = piles.reduce((accumulator, currentValue) => accumulator + Math.ceil(currentValue/middle), 0);
-       
-        if (hoursTaken<=h) {
-            right = middle-1;
+    let hoursTaken = 0;
+
+    while (left < right) {
+        hoursTaken=0;
+        let mid = Math.floor((left+right)/2);
+
+        for (let pile of piles) {
+            hoursTaken += Math.ceil(pile/mid);
         }
+        //if hours taken are less than h e.g 6<=8, then move to left to find smallest value
+        //we want to set right to mid instead of mid-1 bc maybe mid might be the lowest correct val
+        if (hoursTaken <= h) {
+            right = mid;
+        }
+        //if hourstaken is greater than h 10>8 then do mid+1 bc we already know mid is too big discard it
         else {
-            left = middle+1;
+            left = mid+1;
         }
     }
-    
+
     return left;
 };
