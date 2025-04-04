d013822217a84ffd3baf27a45314f54c24ba63fa
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-04 10:38:42 -0400

    Time: 28 ms (79.69%), Space: 65.9 MB (64.52%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.js
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.js
@@ -2,15 +2,15 @@
  * @param {number[]} w
  */
 var Solution = function(w) {
-    //TC O(log n)
-    //SC O(1)
-    this.cumulativeWeights = [];
-    this.totalWeight = 0;
+    //TC O(n)
+    //O(n)
     this.w = w;
+    this.cumulativeWeight = [];
+    this.totalWeight = 0;
 
-    for (let item of w) {
-        this.totalWeight += item;
-        this.cumulativeWeights.push(this.totalWeight);
+    for (let weight of w) {
+        this.totalWeight += weight;
+        this.cumulativeWeight.push(this.totalWeight);
     }
 };
 
@@ -18,23 +18,29 @@ var Solution = function(w) {
  * @return {number}
  */
 Solution.prototype.pickIndex = function() {
-    //generate number between 0 and w.length-1
-    let r = Math.floor(Math.random() * this.totalWeight);
+    //TC O(log n)
+    //SC O(1)
+    //get random weight
+    let random = Math.random() * this.totalWeight;
 
+    //find probability of random weight
     let left = 0;
-    let right = this.w.length-1;
+    let right = this.cumulativeWeight.length-1;
 
-    while (left < right) {
+    while (left<right) {
         let mid = Math.floor((left+right)/2);
 
-        if (this.cumulativeWeights[mid] <= r) {  // Fix: Use `<=` for inclusivity.
-            left = mid + 1;
-        } else {
-            right = mid;
+        if (random > this.cumulativeWeight[mid]) {
+            left = mid+1;
+        }
+        else {
+            right = mid
         }
     }
 
     return left;
+
+
 };
 
 /** 
