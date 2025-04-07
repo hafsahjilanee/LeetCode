e00ad031825c99bd87817271c672ef94e9ab5737
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-06 22:43:10 -0400

    Time: 0 ms (100%), Space: 53.6 MB (43.68%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    //time is O(logn)
-    //space is O(1)
-    let left =0;
+    //TC Olog n
+    //SC O(1)
+    let left = 0;
     let right = nums.length-1;
-    let middle;
 
     while (left<right) {
-        middle = Math.floor((left+right)/2);
-        //part of an incr sequence so the peak must be on the right
-        if (nums[middle]<nums[middle+1]) {
-            left = middle+1;
+        let mid = Math.floor((left+right)/2);
+
+        if (nums[mid]<nums[mid+1]) {
+            left = mid+1;
         }
         else {
-            right=middle;
+            right = mid;
         }
     }
 
