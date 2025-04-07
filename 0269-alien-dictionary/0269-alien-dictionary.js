53172a5748c1f8ac4d3246ee428093a426f8962a
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-07 16:40:00 -0400

    Time: 1 ms (95.5%), Space: 54.5 MB (78.55%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.js
+++ 0269-alien-dictionary/0269-alien-dictionary.js
@@ -2,50 +2,49 @@
  * @param {string[]} words
  * @return {string}
  */
-var alienOrder = function(words) {
+var alienOrder = function (words) {
+    //TC O(C), where C is the total number of characters across all words.
+    //SC O(V + E), where:
+    //V = Number of unique characters (vertices).
+    //E = Number of edges (dependencies between characters)
     let graph = new Map();
 
-    //create new Set for each char
+    //create graph of each char
     for (let word of words) {
         for (let char of word) {
-            if (!graph.has(char)) {
-                graph.set(char, new Set());
-            }
+            graph.set(char, new Set());
         }
     }
 
-    //build the graph by comparing adj words
-    for (let i=0; i<words.length-1; i++) {
+    //map out graph by word[i] and word[i+1]
+    for (let i = 0; i < words.length - 1; i++) {
         let word1 = words[i];
-        let word2 = words[i+1];
+        let word2 = words[i + 1];
 
-        //case where we have apple and then app
-        if (word1.length>word2.length && word1.startsWith(word2)) {
+        //apple and app
+        if (word1.length > word2.length && word1.startsWith(word2)) {
             return "";
         }
-        
-        for (let j=0; j<Math.min(word1.length, word2.length); j++) {
+
+        for (let j = 0; j < Math.min(word1.length, word2.length); j++) {
             let char1 = word1[j];
             let char2 = word2[j];
 
+            //we only care where there is a difference
             if (char1 !== char2) {
-                //wrt and wrf
-                //then add f to t 
-                // t => f
                 graph.get(char1).add(char2);
                 break;
             }
         }
     }
 
-    //topological sort
-    let visiting = new Set(); //currently visiting nodes
-    let visited = new Set(); //fully processed nodes
+    let visiting = new Set();
+    let visited = new Set();
     let res = [];
 
     let dfs = (node) => {
-        if (visited.has(node)) return true; //already been processed skip
-        if (visiting.has(node)) return false; //cycle detected
+        if (visiting.has(node)) return false; //cycle
+        if (visited.has(node)) return true; //skip
 
         visiting.add(node);
 
@@ -61,11 +60,9 @@ var alienOrder = function(words) {
         return true;
     }
 
-    for (let char of graph.keys()) {
-        if (!visited.has(char)) {
-            if (!dfs(char)) {
-                return "";
-            }
+    for (let key of graph.keys()) {
+        if (!visited.has(key) && !dfs(key)) {
+            return "";
         }
     }
 
