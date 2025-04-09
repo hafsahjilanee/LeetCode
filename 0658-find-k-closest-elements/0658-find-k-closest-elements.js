efed559419ac96e6ac9d32b4e44f3450b50b163e
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-09 00:37:20 -0400

    Time: 2 ms (53.08%), Space: 60.4 MB (72.15%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.js
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -5,6 +5,28 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
+
+    // //use binary search if you will have no duplicate values
+    // let left = 0;
+    // let right = arr.length-k
+
+    // while (left< right) {
+    //     let mid = Math.floor((left+right)/2);
+
+    //     if (Math.abs(arr[mid]-x) >Math.abs(arr[mid+k]-x)) {
+    //         left = mid+1;
+    //     }
+    //     else {
+    //         right = mid;
+    //     }
+    // }
+
+    // return arr.slice(left, left+k);
+
+
+
+    //O(N) TC
+    //O(1) sc
     let left = 0;
     let right = arr.length -1;
 
