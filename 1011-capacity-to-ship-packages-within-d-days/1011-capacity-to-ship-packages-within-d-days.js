624c9350af86bd9fc5234904af3ebdcad3a8e5a7
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-09 01:06:35 -0400

    Time: 10 ms (91.88%), Space: 59.3 MB (50.21%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -4,28 +4,19 @@
  * @return {number}
  */
 var shipWithinDays = function(weights, days) {
-    //TC 0(n log n)
-    //SC O(1)
+    let left = Math.max(...weights);
+    let right = weights.reduce((acc,curr)=> acc+curr, 0);
+    let daysTaken = 1;
 
-    //min weight is the max of the array bec that is the min that can be selected. 
-    //anything less than 10 means, 10 will never be able to be shipped
-
-    //max is gonna be sum of all weights bc thats way everyth can be shipped on a single day
-     
-     let left = Math.max(...weights);
-     let right = weights.reduce((acc, curr) => acc+curr, 0);
-     //least acceptable day is 1, cant ship everyth in 0 days
-     let daysTaken = 1;
-
-     while (left< right) {
+    while (left<right) {
         let mid = Math.floor((left+right)/2);
-        daysTaken =1;
-        let sum =0;
-        for (let w of weights) {
-            sum+= w;
-            if (sum> mid) {
+        let sum = 0;
+        daysTaken = 1
+        for (let weight of weights) {
+            sum+= weight;
+            if (sum>mid) {
+                sum = weight;
                 daysTaken++;
-                sum=w;
             }
         }
 
@@ -36,5 +27,6 @@ var shipWithinDays = function(weights, days) {
             left = mid+1;
         }
      }
+
     return left;
 };
