558487f1af362e19dc4853e210b8aea3c9191b9b
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-10 13:45:56 -0400

    Time: 0 ms (100%), Space: 54.1 MB (17.31%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.js
+++ 0670-maximum-swap/0670-maximum-swap.js
@@ -2,22 +2,18 @@
  * @param {number} num
  * @return {number}
  */
-var maximumSwap = function (num) {
-    if (num <= 11) {
-        return num;
-    }
+var maximumSwap = function(num) {
+    if (num<11) return num;
 
     let numsArr = num.toString().split('');
 
-    let maxRight = numsArr[numsArr.length - 1];
-    let maxRightIndex = numsArr.length - 1;
-
+    let maxRight = numsArr[numsArr.length-1];
+    let maxRightIndex = numsArr.length-1;
     let swap_i = -1;
     let swap_j = -1;
 
-    //find max from right
-    for (let right = numsArr.length - 2; right >= 0; right--) {
-        if (numsArr[right] > maxRight) {
+    for (let right= numsArr.length-2; right>=0; right--) {
+        if (numsArr[right]> maxRight) {
             maxRight = numsArr[right];
             maxRightIndex = right;
         }
@@ -26,8 +22,8 @@ var maximumSwap = function (num) {
             [swap_i, swap_j] = [right, maxRightIndex];
         }
     }
-    [numsArr[swap_i], numsArr[swap_j]] = [numsArr[swap_j], numsArr[swap_i]]
 
-    return Number(numsArr.join(''));
+    [numsArr[swap_i], numsArr[swap_j]] = [numsArr[swap_j], numsArr[swap_i]];
 
+    return parseInt(numsArr.join(''));
 };
