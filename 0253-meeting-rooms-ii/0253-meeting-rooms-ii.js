f990523cbf3b710875eb91959aa4920a6d5a1d41
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-10 16:36:49 -0400

    Time: 9 ms (59.13%), Space: 57.8 MB (58.08%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -2,29 +2,26 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-var minMeetingRooms = function (intervals) {
-    //n log n TC
-    //n SC
-    const starts = intervals.map(interval => interval[0]).sort((a, b) => a - b);
-    const ends = intervals.map(interval => interval[1]).sort((a, b) => a - b);
-
-    let res = 0;
-    let count = 0;
+var minMeetingRooms = function(intervals) {
+    let startTimes = intervals.map((e)=> e[0]).sort((a,b)=>a-b);
+    let endTimes = intervals.map((e)=> e[1]).sort((a,b)=>a-b);
 
     let s = 0;
     let e = 0;
+    let count = 0;
+    let res = 0;
 
-    while (s < intervals.length) {
-        if (starts[s] < ends[e]) {
+    while (s<startTimes.length) {
+        if (startTimes[s]< endTimes[e]) {
             s++;
             count++;
         }
         else {
             e++;
-            count--;
+            count--
         }
 
-        res = Math.max(res, count);
+        res =Math.max(res, count);
     }
 
     return res;
