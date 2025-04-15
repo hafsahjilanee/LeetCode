aff2f300ee56c0ef84b9956279ff4612c384d5ff
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-15 00:16:01 -0400

    Time: 192 ms (25.47%), Space: 88.2 MB (70.73%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.js
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.js
@@ -1,8 +1,6 @@
 
 var MedianFinder = function() {
-    //elements lesser than median
     this.lo = new MaxPriorityQueue();
-    //elements higher than the median
     this.hi = new MinPriorityQueue();
 };
 
@@ -11,13 +9,11 @@ var MedianFinder = function() {
  * @return {void}
  */
 MedianFinder.prototype.addNum = function(num) {
-    // First, always add to the max-heap (lo)
+    //TC Olog n per heap insertion
     this.lo.enqueue(num);
-    
-    // Then balance by moving largest from lo to hi
+
     this.hi.enqueue(this.lo.dequeue());
-    
-    // If hi becomes larger, move smallest back to lo
+
     if (this.hi.size() > this.lo.size()) {
         this.lo.enqueue(this.hi.dequeue());
     }
@@ -27,9 +23,11 @@ MedianFinder.prototype.addNum = function(num) {
  * @return {number}
  */
 MedianFinder.prototype.findMedian = function() {
-    if (this.lo.size() === this.hi.size()) {
-        return (this.hi.front() + this.lo.front()) / 2;
-    } else {
+    //O(1) per peek
+    if (this.hi.size() === this.lo.size()) {
+        return (this.hi.front()+this.lo.front())/2;
+    }
+    else {
         return this.lo.front();
     }
 };
