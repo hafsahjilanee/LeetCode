fd4dec055e38c571f24b53df217579887625091a
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-15 20:43:50 -0400

    Time: 0 ms (100%), Space: 53.7 MB (72.29%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,25 +11,25 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    //TC O(n)
-    //SC O(1)
-    //using fast and slow pointer aka two pointers 
+    //tc o(n)
+    //sc O(1)
+    //we need dummy for the case the node we need to remove is the head node
     let dummy = new ListNode(-1, head);
     let left = dummy;
     let right = head;
+
+    //ensure distance between left and right is 2
     while (n>0 && right) {
-        //this will ensure that the difference between slow and fast is always 2 
         right = right.next;
         n--;
     }
 
     while (right) {
+        right = right.next;
         left = left.next;
-        right = right.next; 
     }
 
-    //delet the node 
-    left.next = left.next.next; 
+    left.next = left.next.next;
+
     return dummy.next;
-     
 };
