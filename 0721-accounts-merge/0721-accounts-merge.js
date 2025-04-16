c5b63e9a25c8a9c0a9b7a72963e7aa7dec5975a4
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-16 17:18:31 -0400

    Time: 55 ms (48.17%), Space: 71.9 MB (25.73%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.js
+++ 0721-accounts-merge/0721-accounts-merge.js
@@ -2,49 +2,51 @@
  * @param {string[][]} accounts
  * @return {string[][]}
  */
-var accountsMerge = function (accounts) {
+var accountsMerge = function(accounts) {
+    //N = #accounts, K = avg #emails per account. DFS is O(NK), and sorting is O(NK log NK).
+    //O(NK) Storing the graph and visited emails.
     let graph = new Map();
     let emailToName = {};
 
-    //treat each email as a node and create a map of the other nodes(emails) it's connected to
-
     for (const account of accounts) {
-        const name = account[0];
+        let name = account[0];
 
-        for (let i = 1; i < account.length; i++) {
-            let email = account[i]
-            if (!graph.has(email)) {
-                graph.set(email, new Set());
+        for (let i=1; i<account.length; i++) {
+            
+            if (!graph.has(account[i])) {
+                graph.set(account[i], new Set());
             }
 
-            graph.get(account[1]).add(email);
-            graph.get(email).add(account[1]);
-            emailToName[email] = name;
+            graph.get(account[1]).add(account[i]);
+            graph.get(account[i]).add(account[1]);
+
+            emailToName[account[i]] = name;
         }
     }
 
-    //run dfs
+    //do dfs from each node of graph.keys
     let visited = new Set();
     let res = [];
 
-    for (const email of graph.keys()) {
+    for (let email of graph.keys()) {
         if (!visited.has(email)) {
-            let stack = [email];
             visited.add(email);
             let localRes = [];
+            let stack = [email];
 
             while (stack.length) {
                 let node = stack.pop();
                 localRes.push(node);
 
-                for (const edge of graph.get(node)) {
+                for (let edge of graph.get(node)) {
                     if (!visited.has(edge)) {
-                        stack.push(edge);
                         visited.add(edge);
+                        stack.push(edge);
                     }
                 }
             }
-            res.push([emailToName[email], ...localRes.sort()]);
+
+            res.push([emailToName[email], ...localRes.sort()])
         }
     }
 
