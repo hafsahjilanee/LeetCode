5b6816e8024d5c802633d5985fedf85e49f6902f
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-17 12:32:23 -0400

    Time: 43 ms (5.7%), Space: 64 MB (10.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,26 +4,49 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    if (!nums.length) return [];
+    // if (!nums.length) return [];
 
+    // const freqMap = new Map();
+
+    // for (let num of nums) {
+    //     freqMap.set(num, (freqMap.get(num)||0) +1);
+    // }
+
+    // const bucket = new Array(nums.length+1).fill(null).map(()=> []);
+
+    // for (let [nums, freq] of freqMap) {
+    //     bucket[freq].push(nums);
+    // }
+    // let res = [];
+    // for (let i=bucket.length-1; i>=0; i--) {
+    //     if (res.length===k) {
+    //         return res;
+    //     }
+    //     if (bucket[i].length) {
+    //         res.push(...bucket[i]);
+    //     }
+    // }
+
+
+    if (!nums.length) return [];
+    let maxHeap = new MinPriorityQueue((item) => item[0])
     const freqMap = new Map();
 
     for (let num of nums) {
         freqMap.set(num, (freqMap.get(num)||0) +1);
     }
 
-    const bucket = new Array(nums.length+1).fill(null).map(()=> []);
+    for (let [num, freq] of freqMap) {
+        maxHeap.enqueue([freq,num]);
 
-    for (let [nums, freq] of freqMap) {
-        bucket[freq].push(nums);
+        if (maxHeap.size()>k) {
+            maxHeap.dequeue()
+        }
     }
     let res = [];
-    for (let i=bucket.length-1; i>=0; i--) {
-        if (res.length===k) {
-            return res;
-        }
-        if (bucket[i].length) {
-            res.push(...bucket[i]);
-        }
+    while (res.length !== k) {
+        res.push(maxHeap.dequeue()[1]);
     }
+
+    return res;
 };
