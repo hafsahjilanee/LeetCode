6696f74d261acc8f58337e55613968d1bdb6e94c
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-18 15:34:29 -0400

    Time: 26 ms (62.98%), Space: 60.5 MB (80.85%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.js
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -3,48 +3,45 @@
  * @return {number}
  */
 var calculate = function(s) {
-    //TC O(n)
-    //SC O(1)
+    s = s.trim();
     let curr_operation = '+';
-    let prev = 0;
     let res = 0;
-    
-    for (let i=0; i<s.length; i++) {
-        if (s[i]===' ') continue;
+    let prev = 0;
+
+    for (let i=0;i<s.length; i++) {
+        if (s[i] === ' ') continue;
         if (!isNaN(s[i])) {
             let num = 0;
-            //extract the entire number
+
+            //extract entire number
             while (i<s.length && s[i] !== ' ' && !isNaN(s[i])) {
                 num = num*10 + parseInt(s[i]);
                 i++;
             }
             i--;
 
-            if (curr_operation === "+") {
+            if (curr_operation === '+') {
                 res += num;
                 prev = num;
             }
-            else if (curr_operation === "-") {
+            else if (curr_operation === '-') {
                 res -= num;
                 prev = -num;
             }
-
-            else if (curr_operation === "*") {
+            else if (curr_operation === '*') {
                 res -= prev;
-                prev = prev*num;
+                prev *= num;
                 res += prev;
             }
-            else if (curr_operation === "/") {
+            else if (curr_operation === '/') {
                 res -= prev;
                 prev = Math.trunc(prev/num);
                 res += prev;
-
             }
         }
         else {
             curr_operation = s[i];
         }
     }
-
     return res;
 };
