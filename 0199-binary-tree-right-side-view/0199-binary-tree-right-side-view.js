f1af6ed1f5570293881e6418ffc1f6763d70bfe7
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-19 10:44:56 -0400

    Time: 0 ms (100%), Space: 56.8 MB (5.83%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -25,17 +25,18 @@ var rightSideView = function(root) {
         for (let i=0; i<qLen; i++) {
             let node = q.shift();
 
-            if (i===qLen-1) {
+            if (i === qLen-1) {
                 res.push(node.val);
             }
 
             if (node.left) {
-                q.push(node.left)
+                q.push(node.left);
             }
             if (node.right) {
                 q.push(node.right);
             }
         }
     }
+
     return res;
 };
