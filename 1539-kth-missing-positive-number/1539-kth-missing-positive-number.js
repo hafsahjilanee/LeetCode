2646ca7cda364669608ff98b9cd3b8f5c73d73ad
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-20 12:11:54 -0400

    Time: 1 ms (37.54%), Space: 55.7 MB (11.53%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -3,39 +3,36 @@
  * @param {number} k
  * @return {number}
  */
-var findKthPositive = function (arr, k) {
-    //TC O(n) in worst case we'll have to iterate over all elements
-    //SC O(1) we only using a few extra variables
+var findKthPositive = function(arr, k) {
+    
     if (arr[0] !== 1) {
-        //e.g if k=3 and arr [5,6,7] then result will be 3
-        if (arr[0] - 1 >= k) {
-            return k;
+        //[5,6,7] and k=3
+        if (arr[0]-1 >=k) {
+            return k
         }
         else {
-            //find missing k elements
-            k -= arr[0] - 1;
+            k-= arr[0]-1;
         }
     }
 
-    let i = 0;
+    let i =0;
 
-    while (i < arr.length - 1) {
-        let diff = arr[i + 1] - arr[i];
+    while (i<arr.length) {
+        let diff = arr[i+1] - arr[i];
 
         if (diff !== 1) {
-            for (let j = arr[i] + 1; j < arr[i + 1]; j++) {
-                //k stores how many more elements we need to find 
-                k -= 1;
+             for (let j=arr[i] +1; j<arr[i+1]; j++) {
+                k-=1;
 
                 if (!k) {
                     return j;
                 }
-            }
+             }
         }
         i++;
     }
 
     if (k) {
-        return arr[arr.length - 1] + k
+        return arr[arr.length-1] +k;
     }
 };
