45899224dd94c8779807ec77283cc9d4913ae344
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-20 16:08:00 -0400

    Time: 6 ms (97.47%), Space: 63.6 MB (41.16%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -5,12 +5,12 @@
 var removeDuplicates = function(s) {
     let stack = [];
 
-    for (let i =0; i<s.length; i++) {
-        if (stack.length>0 && stack[stack.length-1] === s[i]) {
+    for (let char of s) {
+        if (stack.length && stack[stack.length-1] === char) {
             stack.pop();
         }
         else {
-            stack.push(s[i]);
+            stack.push(char);
         }
     }
 
