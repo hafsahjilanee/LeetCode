433cfe416b68e999b50c0c78ad9385cbe62f8d48
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-21 22:27:20 -0400

    Time: 72 ms (42.66%), Space: 64.6 MB (63.64%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number[]} to_delete
+ * @return {TreeNode[]}
+ */
+var delNodes = function (root, to_delete) {
+    let toDelete = new Set(to_delete);
+    let forest = [];
+
+    let dfs = (node, isRoot) => {
+        if (!node) return;
+
+        if (toDelete.has(node.val)) {
+            dfs(node.left, true);
+            dfs(node.right, true);
+        }
+        else {
+            if (node.left) {
+                if (toDelete.has(node.left.val)) {
+                    dfs(node.left, true);
+                    node.left = null;
+                }
+                else {
+                    dfs(node.left, false)
+                }
+            }
+            if (node.right) {
+                if (toDelete.has(node.right.val)) {
+                    dfs(node.right, true);
+                    node.right = null;
+                }
+                else {
+                    dfs(node.right, false);
+                }
+            }
+            if (isRoot) {
+                forest.push(node);
+            }
+        }
+    }
+
+    dfs(root, true);
+    return forest;
+};
