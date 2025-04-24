6bb6dbda1c1a5c1f5149920ba74a3d5944d7de62
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-23 20:51:55 -0400

    Time: 76 ms (29.02%), Space: 61.3 MB (52.85%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.js
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.js
@@ -1,7 +1,9 @@
 /**
  * @param {number} n
  */
-var TicTacToe = function (n) {
+var TicTacToe = function(n) {
+    //tc O(n) for initializing array
+    //sc 0(n)
     this.n = n;
     this.rows = new Array(n).fill(0);
     this.cols = new Array(n).fill(0);
@@ -15,10 +17,10 @@ var TicTacToe = function (n) {
  * @param {number} player
  * @return {number}
  */
-TicTacToe.prototype.move = function (row, col, player) {
-    //TC O(1) per move, as we're just updating and checking a few variables.
-    //SC 0(N) bc we are using rows and cols array which will have size n
-    let add = player === 1 ? 1 : -1;
+TicTacToe.prototype.move = function(row, col, player) {
+    //tc 0(1)
+    //sc O(1)
+    let add = player === 1 ? 1: -1;
 
     this.rows[row] += add;
     this.cols[col] += add;
@@ -26,19 +28,19 @@ TicTacToe.prototype.move = function (row, col, player) {
     if (row === col) {
         this.diag += add;
     }
-    if (row + col === this.n - 1) {
+
+    if (row + col === this.n-1) {
         this.antiDiag += add;
     }
 
-    //check if count is 3/-3 for any row. col, diag, antiDiag
+    //check for +3/-3
 
-    if (Math.abs(this.rows[row]) === this.n || 
-    Math.abs(this.cols[col]) === this.n || 
-    Math.abs(this.diag) === this.n || 
+    if (Math.abs(this.rows[row]) === this.n ||
+    Math.abs(this.cols[col]) === this.n ||
+    Math.abs(this.diag) === this.n ||
     Math.abs(this.antiDiag) === this.n) {
-        return player;
-    }
-    else {
+        return player
+    }else {
         return 0;
     }
 };
