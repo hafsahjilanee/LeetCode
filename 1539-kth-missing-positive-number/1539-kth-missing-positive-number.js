8ddf91e20cc56b31c04aaece4944fcc4e6a09fbe
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-04-24 09:13:01 -0400

    Time: 0 ms (100%), Space: 53.8 MB (59.96%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -4,37 +4,59 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
-    //O(n) tc
-    //sc O(1)
+    // //O(n) tc
+    // //sc O(1)
     
-    if (arr[0] !== 1) {
-        //[5,6,7] and k=3
-        if (arr[0]-1 >=k) {
-            return k
+    // if (arr[0] !== 1) {
+    //     //[5,6,7] and k=3
+    //     if (arr[0]-1 >=k) {
+    //         return k
+    //     }
+    //     else {
+    //         k-= arr[0]-1;
+    //     }
+    // }
+
+    // let i =0;
+
+    // while (i<arr.length) {
+    //     let diff = arr[i+1] - arr[i];
+
+    //     if (diff !== 1) {
+    //          for (let j=arr[i] +1; j<arr[i+1]; j++) {
+    //             k-=1;
+
+    //             if (!k) {
+    //                 return j;
+    //             }
+    //          }
+    //     }
+    //     i++;
+    // }
+
+    // if (k) {
+    //     return arr[arr.length-1] +k;
+    // }
+
+    ////////////////////////////////////////////////////
+    //0(log n) approach
+
+    let left =0;
+    let right = arr.length-1;
+
+    while (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        // 4-(2+1) = 1
+        let missing = arr[mid] - (mid+1);
+
+        if (k > missing) {
+            left = mid+1
         }
         else {
-            k-= arr[0]-1;
+            right = mid-1;
         }
     }
-
-    let i =0;
-
-    while (i<arr.length) {
-        let diff = arr[i+1] - arr[i];
-
-        if (diff !== 1) {
-             for (let j=arr[i] +1; j<arr[i+1]; j++) {
-                k-=1;
-
-                if (!k) {
-                    return j;
-                }
-             }
-        }
-        i++;
-    }
-
-    if (k) {
-        return arr[arr.length-1] +k;
-    }
+    //left is the position where the sequence "should" be if no numbers were missing.
+    //Adding k accounts for the k missing numbers skipped before reaching left.
+    return left +k;
 };
