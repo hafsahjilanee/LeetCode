1e60d1ae4f88b2f736d6a5be02d1fcc38d26ab26
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-05-11 13:20:40 -0400

    Time: 1 ms (85.88%), Space: 55.7 MB (89.28%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -3,19 +3,20 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
-    if (n===0) return true;
-    flowerbed.unshift(0);
-    flowerbed.push(0);
-    for (let i=1; i<flowerbed.length-1; i++) {
-        if (flowerbed[i-1]===0 && flowerbed[i+1] === 0 && flowerbed[i]===0) {
+var canPlaceFlowers = function (flowerbed, n) {
+    if (n === 0) return true;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0 &&
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
             //plant a flower
             flowerbed[i] = 1;
             n--;
         }
 
-        if (n===0) return true;
+        if (n === 0) return true;
     }
 
-    return n===0?? false;
+    return n === 0 ?? false;
 };
