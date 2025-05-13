3cc63ecce9315e43b1d88f4a66cef48c2fb39a81
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-05-13 16:38:13 -0400

    Time: 19 ms (99.99%), Space: 54.2 MB (36.55%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -6,16 +6,16 @@
 var mergeAlternately = function(word1, word2) {
     let i = 0;
     let j = 0;
-    let res = [];
+    let res = '';
 
     while (i<word1.length || j<word2.length) {
         if (i<word1.length) {
-            res.push(word1[i++]);
+            res+=word1[i++];
         }
         if (j<word2.length) {
-            res.push(word2[j++]);
+            res+= word2[j++];
         }
     }
 
-    return res.join('');
+    return res;
 };
