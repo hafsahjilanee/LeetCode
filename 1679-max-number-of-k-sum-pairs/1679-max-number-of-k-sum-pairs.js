f5262e032740efb3bdeb05c9222e230be6e66492
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-05-16 18:32:05 -0400

    Time: 79 ms (89.77%), Space: 69.6 MB (35.37%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    let map = new Map();
+    let count = 0;
+
+    for (let i=0; i<nums.length; i++) {
+        let complement = k-nums[i];
+
+        if (map.get(complement)>0) {
+            map.set(complement, map.get(complement) - 1);
+            count++;
+        }
+        else {
+            map.set(nums[i], (map.get(nums[i]) || 0) +1);
+        }
+    }
+
+    return count;
+};
