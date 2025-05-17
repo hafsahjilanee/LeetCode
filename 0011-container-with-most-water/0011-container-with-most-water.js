67b7bae02184fc3a09957e56cf5622ae57278555
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-05-16 23:02:48 -0400

    Time: 3 ms (55.7%), Space: 61.9 MB (78.81%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -2,31 +2,24 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    //time is O(N)
-    //space is O(1) bc we only iterate through the array once
-
+var maxArea = function (height) {
     let left = 0;
-    let right = height.length-1;
-    let maxArea = -Infinity;
+    let right = height.length - 1;
+    let area = -Infinity
 
-    while (left<right) {
-        //area = height of the min column * (base which is right index - left index)
-        //we take height of the min column bc water cannot go above that, it is limited by the lower height col
+    while (left < right) {
+        //bc minHeight determines the rectangle formed
+        let minHeight = Math.min(height[left], height[right]);
+        area = Math.max(area, minHeight * (right - left));
 
-        let area = Math.min(height[left], height[right]) * (right-left);
-
-        maxArea = Math.max(maxArea,area);
-
-        //we move pointer from the short height bc area is limited by the short height
         if (height[left] < height[right]) {
             left++;
         }
         else {
             right--;
         }
-
     }
 
-    return maxArea;
+    return area;
+
 };
