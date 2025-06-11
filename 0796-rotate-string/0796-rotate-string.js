f8ce30d7c28ea9529b20722e47d840908d4df7e4
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-06-11 17:34:27 -0400

    Time: 0 ms (100%), Space: 52.6 MB (94.21%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -3,22 +3,25 @@
  * @param {string} goal
  * @return {boolean}
  */
-var rotateString = function(s, goal) {
+var rotateString = function (s, goal) {
+    // if (s.length !== goal.length) return false;
+    // let rotatedWord = s;
+
+    // for (let i = 0; i < s.length; i++) {
+    //     rotatedWord = rotateOnce(rotatedWord)
+
+    //     if (rotatedWord === goal) return true;
+    // }
+
+    // return false;
+
     if (s.length !== goal.length) return false;
-    let rotatedWord = s;
-
-    for (let i=0; i<s.length; i++) {
-        rotatedWord = rotateOnce(rotatedWord)
-
-        if (rotatedWord === goal) return true;
-    }
-
-    return false;
+    return (s + s).includes(goal);
 };
 
-var rotateOnce = function(word) {
-    let char = word[0];
-    word = word.slice(1);
-    word += char;
-    return word;
-}
+// var rotateOnce = function (word) {
+//     let char = word[0];
+//     word = word.slice(1);
+//     word += char;
+//     return word;
+// }
