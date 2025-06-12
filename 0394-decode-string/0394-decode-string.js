c658df65c6f066d7df88e125f469f9c23ebf6806
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-06-12 14:22:48 -0400

    Time: 46 ms (4.36%), Space: 53.2 MB (69.09%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let stack = [];
+    let currentNum = 0;
+    let currentStr = "";
+
+    for (let char of s) {
+        if (!isNaN(char)) {
+            currentNum = currentNum * 10 + parseInt(char);
+        } else if (char === "[") {
+            stack.push([currentStr, currentNum]);
+            currentStr = "";
+            currentNum = 0;
+        } else if (char === "]") {
+            let [prevStr, num] = stack.pop();
+            currentStr = prevStr + currentStr.repeat(num);
+        } else {
+            currentStr += char;
+        }
+    }
+
+    return currentStr;
+};
