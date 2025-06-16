5f9c2cedbc7f2fdbb61d8abd649fa54fa17d96b1
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-06-16 01:52:01 -0400

    Time: 7 ms (49.8%), Space: 57.5 MB (73.57%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -3,28 +3,32 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    const ROWS = grid.length, COLS = grid[0].length;
-    //const directions = [[1,0], [-1,0], [0,1], [0,-1]];
+    let directions = [[-1,0], [1,0], [0,1], [0,-1]];
 
-    const dfs = (r,c) => {
-        if (r<0 || c<0 || r===ROWS || c===COLS || grid[r][c]===0) {
-            return 0;
+    let ROWS = grid.length;
+    let COLS = grid[0].length;
+
+    let dfs = (r,c) => {
+        if (Math.min(r,c)<0 || r===ROWS || c===COLS || grid[r][c] === 0) return 0;
+
+        //mark as visited
+        grid[r][c] = 0;
+        let area = 1;
+        for (let [dr, dc] of directions) {
+            area += dfs(r+dr, c+dc);
         }
 
-        grid[r][c] =0;
-
-        //1 is the current and then we'll add 1 if there are any adjacent blocks
-        return 1 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1);
+        return area;
     }
 
-    let area = 0;
+    let maxArea = 0;
+    let islandArea = 0;
+
     for (let r=0; r<ROWS; r++) {
         for (let c=0; c<COLS; c++) {
-            if (grid[r][c] === 1) {
-                area = Math.max(area, dfs(r,c));
-            }
+            maxArea = Math.max(maxArea, dfs(r,c));
         }
     }
 
-    return area;
+    return maxArea;
 };
