f2e08f3dd2cd9bbd0d59042f511549bb720f926d
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-06-16 09:52:32 -0400

    Time: 0 ms (100%), Space: 65.5 MB (49.36%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    let totalTank = 0;
+    let currTank = 0; 
+    let start = 0
+
+    for (let i=0; i<gas.length; i++) {
+        totalTank += gas[i] - cost[i];
+        currTank += gas[i] - cost[i];
+
+        if (currTank<0) {
+            start = i+1;
+            currTank = 0;
+        }
+    }
+
+    return totalTank >=0 ? start: -1;
+};
