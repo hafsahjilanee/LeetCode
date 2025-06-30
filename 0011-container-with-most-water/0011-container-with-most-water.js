6bdeba0c8630e338d2a63c9299cae0444bec8155
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-06-30 17:21:17 -0400

    Time: 2 ms (81.23%), Space: 63.6 MB (11.06%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -2,24 +2,21 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {
+var maxArea = function(height) {
     let left = 0;
-    let right = height.length - 1;
-    let area = -Infinity
+    let right = height.length-1;
+    let maxArea = 0;
 
-    while (left < right) {
-        //bc minHeight determines the rectangle formed
-        let minHeight = Math.min(height[left], height[right]);
-        area = Math.max(area, minHeight * (right - left));
+    while (left<right) {
+        maxArea = Math.max(maxArea, Math.min(height[left], height[right])*(right-left));
 
         if (height[left] < height[right]) {
             left++;
-        }
+        } 
         else {
             right--;
         }
     }
 
-    return area;
-
+    return maxArea;
 };
