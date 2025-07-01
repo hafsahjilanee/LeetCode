2d2186ec2158cdc29d3b13b9f4df16c252411afd
hafsahjilanee <99481843+hafsahjilanee@users.noreply.github.com>
2025-07-01 13:38:07 -0400

    Time: 6 ms (79.13%), Space: 80.9 MB (62.43%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -10,22 +10,58 @@
  * @return {number}
  */
 var pairSum = function(head) {
-    let values = [];
+    // //tc O(n)
+    // //sc O(n)
+    // let values = [];
 
-    let curr = head;
-    let n=0
+    // let curr = head;
+    // let n=0
 
-    while (curr) {
-        values.push(curr.val);
-        curr = curr.next;
-        n++;
+    // while (curr) {
+    //     values.push(curr.val);
+    //     curr = curr.next;
+    //     n++;
+    // };
+
+    // let maxSum = 0;
+
+    // for (let i=0; i<n/2; i++) {
+    //     maxSum = Math.max(maxSum, values[i] + values[n-1-i])
+    // }
+
+    // return maxSum;
+
+    //tc O(n)
+    //sc O(1)
+
+    if (!head) return null;
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
     };
 
-    let maxSum = 0;
+    //reverse second half of linked list
+    let nextNode;
+    let prev = null;
 
-    for (let i=0; i<n/2; i++) {
-        maxSum = Math.max(maxSum, values[i] + values[n-1-i])
+    while (slow) {
+        nextNode = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = nextNode;
     }
 
-    return maxSum;
+    //prev is now the head of reversed list
+    let curr = head;
+    let max = 0;
+    while (prev) {
+        max = Math.max(max, prev.val+curr.val);
+        prev = prev.next;
+        curr = curr.next;
+    }
+
+    return max;
 };
